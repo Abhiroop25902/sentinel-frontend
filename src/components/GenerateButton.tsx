f73bcacae03bac4f9f8a4a3ca5e4d55c88cbe656
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-02-17 01:56:43 +0530

    ADD: Stress Test now works

--- src/components/GenerateButton.tsx
+++ src/components/GenerateButton.tsx
@@ -1,11 +1,41 @@
+import {createResource, createSignal, Match, Switch} from "solid-js";
+import BackendConstants from "../constants/BackendConstants";
+
 export default function GenerateButton() {
+    const [hasBeenClicked, setHasBeenClicked] = createSignal(false);
+    const [stressTest] = createResource(hasBeenClicked, async (clicked) => {
+        if (!clicked) return;
+
+        const res = await fetch(`${BackendConstants.HOST}${BackendConstants.PATHS.STRESS_TEST}`);
+
+        if (res.status === 409) {
+            throw new Error('Currently in Timeout');
+        }
+
+        const data = await res.json();
+        console.log(data);
+        return data;
+    });
+
     return (
         <div class={""}>
             <button
-                class={"bg-blue-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 transition duration-150"}
+                class={"bg-blue-600 px-6 py-3 w-56 h-12 rounded-lg font-semibold flex justify-center text-white"}
+                disabled={hasBeenClicked()}
                 onClick={() => {
+                    setHasBeenClicked(true);
                 }}>
-                Launch Stress Test
+                <Switch fallback={<div>Launch Stress Test</div>}>
+                    <Match when={stressTest.loading}>
+                        <div class="animate-spin rounded-full h-6 w-6 border-2 border-blue-300 border-t-transparent"/>
+                    </Match>
+                    <Match when={stressTest.error}>
+                        <div>Currently in Timeout</div>
+                    </Match>
+                    <Match when={stressTest()}>
+                        <div>Done</div>
+                    </Match>
+                </Switch>
             </button>
         </div>
     )
--- src/constants/BackendConstants.ts
+++ src/constants/BackendConstants.ts
@@ -1,6 +1,7 @@
 export default {
     HOST: 'https://sentinel-backend-git-357338564537.us-central1.run.app',
     PATHS: {
-        HEALTH: '/actuator/health/liveness'
+        HEALTH: '/actuator/health/liveness',
+        STRESS_TEST: '/api/stress-test',
     }
 }
