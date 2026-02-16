7312f605217796f40bf9189fe3d5b9417c34e3d2
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-02-16 21:45:47 +0530

    ADD: status dot works now

--- src/components/StatusDot.tsx
+++ src/components/StatusDot.tsx
@@ -1,5 +1,28 @@
+import BackendConstants from "../constants/BackendConstants";
+import {createResource, Match, Show, Switch} from "solid-js";
+
 export default function StatusDot() {
+    const healthUri = `${BackendConstants.HOST}${BackendConstants.PATHS.HEALTH}`
+    const fetchHealth = async () => {
+        const response = await fetch(healthUri);
+        return await response.json();
+    }
+
+    const [health] = createResource(fetchHealth);
+
     return (
-        <div class={`bg-emerald-500 w-5 h-5 rounded-full`}></div>
+        <div>
+            <Show when={health.loading}>
+                <div class={`border-amber-500 border-2 w-5 h-5 rounded-full`}></div>
+            </Show>
+            <Switch>
+                <Match when={health.error}>
+                    <div class={`bg-red-500 w-5 h-5 rounded-full`}></div>
+                </Match>
+                <Match when={health()}>
+                    <div class={`bg-emerald-500 w-5 h-5 rounded-full`}></div>
+                </Match>
+            </Switch>
+        </div>
     )
 }
---
+++ src/constants/BackendConstants.ts
@@ -0,0 +1,6 @@
+export default {
+    HOST: 'https://sentinel-backend-git-357338564537.us-central1.run.app',
+    PATHS: {
+        HEALTH: '/actuator/health/liveness'
+    }
+}
