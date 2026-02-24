9a7c6a63a7bd5ff2311d64becca46085d64a5acf
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-02-25 01:51:00 +0530

    ADD: stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+    // Extends the standard config for best practices
+    extends: ["stylelint-config-standard"],
+    rules: {
+        "import-notation": "string"
+    },
+};
