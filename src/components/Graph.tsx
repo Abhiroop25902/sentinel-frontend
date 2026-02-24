2b9e3ed42e5e98548a5dd20ed05e1a6c41e0f651
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-02-25 01:54:45 +0530

    FIX: add data visibility UI in KPI.tsx and Graph.tsx, actual UI will come later

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,7 +1,29 @@
+import {setStore, store} from "../store";
+import {createSignal, onCleanup} from "solid-js";
+
+
 export default function Graph() {
+    const [lastSecondSuccessCount, setLastSecondSuccessCount] = createSignal(0);
+    const [lastSecondFailCount, setLastSecondFailCount] = createSignal(0);
+
+    const timer = setInterval(() => {
+        // making separate variable cause store change will happen before setStore callback happens
+        const lastSecondSuccessCount = store.lastSecondCount.success;
+        const lastSecondFailCount = store.lastSecondCount.fail
+        setLastSecondSuccessCount(lastSecondSuccessCount);
+        setLastSecondFailCount(lastSecondFailCount);
+
+        setStore("lastSecondCount", "success", num => num - lastSecondSuccessCount);
+        setStore("lastSecondCount", "fail", num => num - lastSecondFailCount);
+    }, 1000);
+
+    onCleanup(() => clearInterval(timer));
+
     return (
         <div class={`w-full flex items-center justify-center text-gray-300 
-        bg-slate-800 rounded-2xl basis-3 grow-4`}>
-            <p>Graph</p>
-        </div>)
+        bg-slate-800 rounded-2xl basis-3 grow-4 flex-col`}>
+            <div>{`Last Second Success: ${lastSecondSuccessCount()}`}</div>
+            <div>{`Last Second Fail: ${lastSecondFailCount()}`}</div>
+        </div>
+    )
 }
--- src/components/KPI.tsx
+++ src/components/KPI.tsx
@@ -1,6 +1,23 @@
+import {createSignal, onCleanup} from "solid-js";
+import {store} from "../store";
+
 export default function KPI() {
+
+    const [totalLoginSuccessCount, setTotalLoginSuccessCount] = createSignal(0);
+    const [totalLoginFailCount, setTotalLoginFailCount] = createSignal(0);
+
+    const timer = setInterval(() => {
+        setTotalLoginSuccessCount(store.loginCount.success);
+        setTotalLoginFailCount(store.loginCount.fail);
+    }, 1000);
+
+    onCleanup(() => clearInterval(timer));
+
     return (
         <div class={`w-full flex items-center justify-center text-gray-300 
-        bg-slate-800 rounded-2xl grow`}>KPI</div>
+        bg-slate-800 rounded-2xl grow flex-col`}>
+            <div>{`Total Success Count: ${totalLoginSuccessCount()}`}</div>
+            <div>{`Total Fail Count: ${totalLoginFailCount()}`}</div>
+        </div>
     )
 }
