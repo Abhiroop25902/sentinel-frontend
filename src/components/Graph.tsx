d73798cc6371905b71afa5dd83efa063e6042329
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-02 02:21:09 +0530

    FIX: I want the graph to grow in 3:1 between graph and log [grow-3], while not considering inside content of graph to calculate grow (basis-0)

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -21,7 +21,7 @@ export default function Graph() {
 
     return (
         <div class={`w-full flex items-center justify-center text-gray-300 
-        bg-slate-800 rounded-2xl basis-3 grow-4 flex-col`}>
+        bg-slate-800 rounded-2xl basis-0 grow-3 flex-col`}>
             <div>{`Last Second Success: ${lastSecondSuccessCount()}`}</div>
             <div>{`Last Second Fail: ${lastSecondFailCount()}`}</div>
         </div>
