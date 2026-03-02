0f5298614d35809fdaedc3bb8d4eeecc3049d717
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-03 04:17:18 +0530

    ADD: Graph component implementation

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,17 +1,32 @@
 import {setStore, store} from "../store";
 import {createSignal, onCleanup} from "solid-js";
+import {LineChart} from "./SolidUIChart/charts";
+import {formatDate} from "../utils/utils";
+import colors from 'tailwindcss/colors'
 
+const successLabel = "Successful Logins"
+const successColor = colors.emerald[500];
+const failLabel = "Failed Logins"
+const failureColor = colors.rose[500];
+const textColor = colors.gray[300];
 
 export default function Graph() {
-    const [lastSecondSuccessCount, setLastSecondSuccessCount] = createSignal(0);
-    const [lastSecondFailCount, setLastSecondFailCount] = createSignal(0);
+    const [successHistory, setSuccessHistory] = createSignal<Array<number | null>>(Array.from({length: 60}, _ => null));
+    const [failHistory, setFailHistory] = createSignal<Array<number | null>>(Array.from({length: 60}, _ => null));
+    const currentTime = new Date().getTime();
+    const [timeLabel, setTimeLabel] = createSignal(Array.from({length: 60}, (_, i) => {
+        return formatDate(new Date(currentTime - (59 - i) * 1000))
+    }));
+    const [hideSuccessGraph, setHideSuccessGraph] = createSignal(false)
+    const [hideFailGraph, setHideFailGraph] = createSignal(false)
 
     const timer = setInterval(() => {
         // making separate variable cause store change will happen before setStore callback happens
         const lastSecondSuccessCount = store.lastSecondCount.success;
         const lastSecondFailCount = store.lastSecondCount.fail
-        setLastSecondSuccessCount(lastSecondSuccessCount);
-        setLastSecondFailCount(lastSecondFailCount);
+        setSuccessHistory([...successHistory().slice(1), lastSecondSuccessCount]);
+        setFailHistory([...failHistory().slice(1), lastSecondFailCount]);
+        setTimeLabel([...timeLabel().slice(1), formatDate(new Date())])
 
         setStore("lastSecondCount", "success", num => num - lastSecondSuccessCount);
         setStore("lastSecondCount", "fail", num => num - lastSecondFailCount);
@@ -20,10 +35,107 @@ export default function Graph() {
     onCleanup(() => clearInterval(timer));
 
     return (
-        <div class={`w-full flex items-center justify-center text-gray-300 
-        bg-slate-800 rounded-2xl basis-0 grow-3 flex-col`}>
-            <div>{`Last Second Success: ${lastSecondSuccessCount()}`}</div>
-            <div>{`Last Second Fail: ${lastSecondFailCount()}`}</div>
+        <div class={`w-full flex items-center justify-center text-gray-300
+        bg-slate-800 rounded-2xl basis-0 grow-3 flex-col min-h-0`}>
+            <div class={`p-4 w-full h-full`}>
+                <LineChart
+                    data={{
+                        xLabels: timeLabel(),
+                        datasets: [
+                            {
+                                label: successLabel,
+                                data: successHistory(),
+                                borderColor: successColor,
+                                backgroundColor: successColor,
+                                pointBackgroundColor: successColor,
+                                tension: 0.5,
+                                borderWidth: 1.5,
+                                pointStyle: false,
+                                hidden: hideSuccessGraph(),
+
+                            },
+                            {
+                                label: failLabel,
+                                data: failHistory(),
+                                fill: false,
+                                borderColor: failureColor,
+                                backgroundColor: failureColor,
+                                pointBackgroundColor: failureColor,
+                                tension: 0.5,
+                                borderWidth: 1.5,
+                                pointStyle: false,
+                                hidden: hideFailGraph()
+                            }
+                        ],
+                    }}
+                    options={{
+                        animation: false,
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            x: {
+                                border: {display: false},
+                                grid: {display: false},
+                                title: {
+                                    display: true,
+                                    text: "Time",
+                                    color: textColor
+                                },
+                                ticks: {
+                                    color: textColor,
+                                    maxTicksLimit: 6,
+                                    autoSkip: true,
+                                    maxRotation: 0,
+                                    minRotation: 0,
+                                }
+                            },
+                            y: {
+                                border: {
+                                    dash: [3],
+                                    dashOffset: 3,
+                                    display: false
+                                },
+                                grid: {
+                                    color: "hsla(240, 3.8%, 46.1%, 0.4)"
+                                },
+                                title: {
+                                    display: true,
+                                    text: "Logins per Second",
+                                    color: textColor
+                                },
+                                ticks: {
+                                    color: textColor
+                                }
+
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: true,
+                                align: "end",
+                                labels: {
+                                    boxWidth: 6,
+                                    boxHeight: 6,
+                                    color: textColor,
+                                    font: {size: 14},
+                                    usePointStyle: true,
+                                    pointStyle: 'circle',
+                                },
+                                onClick: (_, legendItem) => {
+                                    if (legendItem.text === successLabel) setHideSuccessGraph(!hideSuccessGraph());
+                                    else setHideFailGraph(!hideFailGraph());
+                                }
+
+                            },
+                            tooltip: {
+                                enabled: false,
+                            },
+                        },
+                    }}
+
+                />
+            </div>
+
         </div>
     )
 }
