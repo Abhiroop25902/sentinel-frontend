4df93c992daea7025d195040d0645633821703b7
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2026-03-21 12:52:55 +0530

    fix: fixed localhost url to backend url

--- src/constants/BackendConstants.ts
+++ src/constants/BackendConstants.ts
@@ -1,5 +1,5 @@
 export default {
-    HOST: 'http://localhost:8080',
+    HOST: 'https://sentinel-backend-git-357338564537.us-central1.run.app',
     PATHS: {
         HEALTH: '/actuator/health/liveness',
         STRESS_TEST: '/api/stress-test',
